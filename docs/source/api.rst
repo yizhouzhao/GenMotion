cf2a3b864bc238bf47933b6a1c537065ec82c99a
Yizhou <yizhou_zhao@berkeley.edu>
2021-10-11 13:07:34 -0700

    update module

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -6,3 +6,9 @@ API
 
 #. HELLO
 #. HELLO2
+
+.. automodule:: src.render.c4d.socket_server_in_c4d
+    :members:
+    :private-members:
+    :inherited-members:
+    :show-inheritance:
---
+++ src/render/c4d/socket_server_in_c4d.py
@@ -0,0 +1,56 @@
+import c4d
+import socket
+import time
+
+def SimpleServer(HOST = '127.0.0.1', PORT = 3005):
+    """
+    Generate a simple TCP socket server in Cinema 4D
+
+    Args:
+        HOST (str): local host address;
+        POST (int): server port;
+    """
+    sock = socket.socket()
+    sock.bind((HOST,PORT))
+
+    sock.listen()
+    conn, addr = sock.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(10240).decode("utf-8") 
+            #if not data:
+            #    break
+            if data:
+                try:
+                    print("received:", data) 
+                    exec(data)
+                    
+                    # conn.sendall("".encode()) # data.encode()
+                except Exception as e:
+                    print(e)
+                    print("wrong command:", data)
+                    conn.sendall("wrong command".encode()) # data.encode()
+
+            #time.sleep(1)
+    
+    print("Disconnected")
+
+class UserThread(c4d.threading.C4DThread):
+    """
+    A user thread to hold the socket server in Cinema 4D
+    """
+    def Main(self):
+        # Put in your code here
+        # which you want to run
+        # in another thread
+        print("start server......")
+        SimpleServer()
+
+if __name__ == '__main__':
+
+    # server_thread = threading.C4DThread(target=simpleServer)
+    # server_thread.start()
+
+    thread = UserThread()
+    thread.Start()
