65b6b97dc6915a12f5b30e3f805df93798230889
Yizhou <yizhouzhao@g.ucla.edu>
2021-10-12 18:33:17 -0700

    Maya command comment

--- src/render/maya/utils.py
+++ src/render/maya/utils.py
@@ -81,17 +81,25 @@ class MayaController:
         recv_message = self.SendCommand(send_message)
 
     def MoveObjectWorldRelative(self, object_name: str, location: list):
-        '''
-        Set world relative location for object with location [x, y, z]
-        '''
+        """Move an object relatively with location [x, y, z]
+
+        :param object_name: the name of the object in hierarchy view
+        :type object_name: str
+        :param location: location in list [x, y, z]
+        :type location: list
+        """
         send_message = "select -replace " + object_name + ";"
         send_message += "move -relative " + str(location[0]) + " " + str(location[1]) + " " + str(location[2]) + ";"
         recv_message = self.SendCommand(send_message)
 
     def SetObjectLocalTransform(self, object_name: str, location: list):
-        '''
-        Set world absolute location for object with location [x,y] or [x, y, z]
-        '''
+        """Set world absolute location for object with location [x,y] or [x, y, z]
+
+        :param object_name: the name of the object in hierarchy view
+        :type object_name: str
+        :param location: location in list [x, y, z]
+        :type location: list
+        """
         send_message = "select -replace " + object_name + ";" + "move -relative "
         for value in location:
             send_message += str(value) + " "
@@ -100,9 +108,13 @@ class MayaController:
         recv_message = self.SendCommand(send_message)
 
     def SetObjectLocalRotation(self, object_name: str, rotation: list):
-        '''
-        Set world absolute location for object with rotation [x,y] or [x, y, z] in degree
-        '''
+        """Set world absolute location for object with rotation [x,y] or [x, y, z] in degree
+
+        :param object_name: the name of the object in hierarchy view
+        :type object_name: str
+        :param location: location in list [x, y, z]
+        :type location: list
+        """
         send_message = "select -replace " + object_name + ";" + "rotate -relative "
         for value in rotation:
             send_message += str(value) + "deg "
@@ -111,17 +123,34 @@ class MayaController:
         recv_message = self.SendCommand(send_message)
 
     def SetCurrentKeyFrameForAttribute(self, object_name: str, attr_name: str):
+        """Set keyframe for object attribute
+
+        :param object_name: name of the object in hierarchy view
+        :type object_name: str
+        :param attr_name: name of attribute
+        :type attr_name: list
+        """
         send_message = "select -r " + object_name + ";"
         send_message += "setKeyframe -at " + attr_name + ";"
         recv_message = self.SendCommand(send_message)
 
     def SetCurrentKeyFrameForPositionAndRotation(self, object_name: str):
+        """Set keyframe for object position and rotation
+
+        :param object_name: name of the object in hierarchy view
+        :type object_name: str
+        """
         send_message = "select -r " + object_name + ";"
         send_message += "setKeyframe -at translate;"
         send_message += "setKeyframe -at rotate;"
         recv_message = self.SendCommand(send_message)
 
     def SetCurrentKeyFrameForObjects(self, object_list):
+        """Set keyframe for a list of objects
+
+        :param object_list: a list of object names
+        :type object_list: list
+        """
         send_message = "setKeyframe {"
         for obj in object_list:
             send_message += "\"" + str(obj) + "\", "
@@ -129,22 +158,33 @@ class MayaController:
         recv_message = self.SendCommand(send_message)
 
     def SetObjectAttribute(self, object_name: str, attr_name: str, value: float):
+        """Set object attriture with value
+
+        :param object_name: name of the object
+        :type object_name: str
+        :param attr_name: attribute of the object
+        :type attr_name: str
+        :param value: attribute value
+        :type attr_name: float
+        """
+
         send_message = "setAttr " + object_name + "." + attr_name + " " + str(value) + ";"
         recv_message = self.SendCommand(send_message)
 
-    def SetMultipleAttributes(self, attributes: dict):
-        '''
-        :param attributes: dictionary of facial attributes to be set
-        '''
-        for joint, attr_dict in attributes.items():
-            for name, value in attr_dict.items():
-                self.SetObjectAttribute(joint, name, value)
+    # def SetMultipleAttributes(self, attributes: dict):
+    #     '''
+    #     Set object attriture with value
+
+    #     :param attributes: dictionary of facial attributes to be set
+    #     :type attributes: dict
+    #     '''
+    #     for joint, attr_dict in attributes.items():
+    #         for name, value in attr_dict.items():
+    #             self.SetObjectAttribute(joint, name, value)
 
     def Undo(self):
-        '''
-        Maya undo
-        :return:
-        '''
+        """Send undo command to socket server (Maya)
+        """
         send_message = "undo;"
         rec_message = self.SendCommand(send_message)
         # print(rec_message)
@@ -153,8 +193,9 @@ class MayaController:
     def UndoToBeginning(self, max_step=200):
         '''
         Undo Maya file to beginning
-        :param max_step:
-        :return:
+
+        :param max_step: max undo steps, default 200
+        :type max_step: int, optional
         '''
         for _ in range(max_step):
             rec_message = self.Undo()
@@ -165,9 +206,16 @@ class MayaController:
     def ScreenShot(self, save_file: str, camera="persp", width=1024, height=1024):
         '''
         Take maya screen shot and save to picture
+
         :param save_file: save file name
-        :param camera: camera name
-        :return:
+        :type save_file: str
+        :param camera: camera name in the Maya scene, default "persp"
+        :type camera: str, optional
+        :param width: screenshot width, default 1024
+        :type width: int, optional
+        :param height: screenshot height, default 1024
+        :type height: int, optional
+        
         '''
         send_message = "string $editor = `renderWindowEditor -q -editorName`;\n"
         #send_message += "string $myCamera = " + camera + ";\n"
@@ -182,7 +230,9 @@ class MayaController:
     def GetAllObjects(self):
         '''
         Get all the objects from Maya scene
-        :return: a list containing all the objects in the scene
+        
+        :return recv_message: a list of object names
+        :rtype: list
         '''
         send_message = "ls;"
         recv_message = self.SendCommand(send_message)
@@ -191,7 +241,8 @@ class MayaController:
     def GetTimeSliderRange(self):
         '''
         Get the range of time slider
-        :return: [min, max]
+        :return: [min, max] representing the minimum and maximum values of the timeline
+        :rtype: list
         '''
         send_message = "playbackOptions -q -minTime"
         recv_message_1 = self.SendCommand(send_message)
@@ -205,8 +256,13 @@ class MayaController:
 
     def GetObjectWorldTransform(self, object_name: str):
         '''
-        Get Object world location
-        return: cordindate [x,y,z]
+        Get object world location
+
+        :param object_name: name of the object
+        :type object_name: str
+
+        :return: cordindate [x,y,z] of the object
+        :rtype: list
         '''
         send_message = "xform -q -t -ws " + object_name + ";"
         recv_message = self.SendCommand(send_message)
@@ -216,8 +272,12 @@ class MayaController:
     def GetObjectLocalRoation(self, object_name: str):
         '''
         Get object local rotation
-        :param object_name:
-        :return: cordinate [x, y, z]
+
+        :param object_name: name of the object
+        :type object_name: str
+
+        :return: cordindate [x,y,z] of the object
+        :rtype: list
         '''
         send_message = "xform -q -ro -os " + object_name + ";"
         recv_message = self.SendCommand(send_message)
@@ -226,8 +286,16 @@ class MayaController:
 
     def GetObjectAttribute(self, object_name: str, attr_name: str):
         '''
-        Get attribute value
-        return [float]
+        Get attribute value of an object
+
+        :param object_name: name of the object
+        :type object_name: str
+
+        :param attr_name: name of the attribute
+        :type attr_name: str
+
+        :return: attribute value
+        :rtype: float
         '''
         send_message = "getAttr " + object_name + "." + attr_name + ";"
         recv_message = self.SendCommand(send_message)
@@ -236,8 +304,13 @@ class MayaController:
 
     def GetBodyInformation(self, joint_list: list):
         '''
+        Get a list of information of the joint list
+
         :param joint_list: a list of joint names
+        :type joint_list: list
+
         :return: a list with joint positions
+        :rtype: list
         '''
         BODY_INFO_DIC = {}
         for joint_name in joint_list:
@@ -256,53 +329,54 @@ class MayaController:
         return BODY_INFO_DIC
 
     #--------------------------UTIL-------------------------------------
-    def GetAdvancedSkeletonJointNameFromIndex(self, joint_index: int):
-        '''
-        :param joint_index:
-        :return: advanced skeleton joint name
-        '''
-        return G_Joint_Maya_Advanced_Skeleton[joint_index]
+    # def GetAdvancedSkeletonJointNameFromIndex(self, joint_index: int):
+    #     '''
+        
+    #     :param joint_index:
+    #     :return: advanced skeleton joint name
+    #     '''
+    #     return G_Joint_Maya_Advanced_Skeleton[joint_index]
 
-    def GenerateSceneFromPose(self, loading_path: str, saving_path: str, if_save = False):
-        '''
-        Load a pose from loading_path into the scene and save it to saving_path
-        '''
-        with open(loading_path) as f:
-            data = json.load(f)
-        #print(json.dumps(data, indent = 4, sort_keys=True))
-        for joints, attrs in data["objects"].items():
-            for attr_name, value in attrs["attrs"].items():
-                if isinstance(value["value"], float):
-                    self.SetObjectAttribute(joints, attr_name, value["value"])     
-        if if_save:
-          self.saveFile(saving_path)
+    # def GenerateSceneFromPose(self, loading_path: str, saving_path: str, if_save = False):
+    #     '''
+    #     Load a pose from loading_path into the scene and save it to saving_path
+    #     '''
+    #     with open(loading_path) as f:
+    #         data = json.load(f)
+    #     #print(json.dumps(data, indent = 4, sort_keys=True))
+    #     for joints, attrs in data["objects"].items():
+    #         for attr_name, value in attrs["attrs"].items():
+    #             if isinstance(value["value"], float):
+    #                 self.SetObjectAttribute(joints, attr_name, value["value"])     
+    #     if if_save:
+    #       self.saveFile(saving_path)
         
-    def SaveFile(self, saving_path: str):
-        '''
-        Save file to the specified directory
-        '''
-        def save(saving_path):
-            send_message = "file -rename \"%s\"; file -save -type \"mayaBinary\"" % saving_path
-            rec_message = self.SendCommand(send_message)
-            return rec_message
+    # def SaveFile(self, saving_path: str):
+    #     '''
+    #     Save file to the specified directory
+    #     '''
+    #     def save(saving_path):
+    #         send_message = "file -rename \"%s\"; file -save -type \"mayaBinary\"" % saving_path
+    #         rec_message = self.SendCommand(send_message)
+    #         return rec_message
         
-        save_thread = threading.Thread(target=save, args=(saving_path,))
-        save_thread.start()
-        time.sleep(1)
-        press("enter")
-        time.sleep(1)
+    #     save_thread = threading.Thread(target=save, args=(saving_path,))
+    #     save_thread.start()
+    #     time.sleep(1)
+    #     press("enter")
+    #     time.sleep(1)
        
-    def GenerateSceneFromPoseWithIdentifiers(self, loading_path: str, identifiers: list):
-        '''
-        Load a pose from loading_path into the scene and save it to saving_path
-        params: loading_path: json path
-        identifiers list(str): joints names
-        '''
-        with open(loading_path) as f:
-            data = json.load(f)
-        # print(json.dumps(data, indent = 4, sort_keys=True))
-        for joints, attrs in data["objects"].items():
-            if joints in identifiers:
-                for attr_name, value in attrs["attrs"].items():
-                    if isinstance(value["value"], float):
-                        self.SetObjectAttribute(joints, attr_name, value["value"])
+    # def GenerateSceneFromPoseWithIdentifiers(self, loading_path: str, identifiers: list):
+    #     '''
+    #     Load a pose from loading_path into the scene and save it to saving_path
+    #     params: loading_path: json path
+    #     identifiers list(str): joints names
+    #     '''
+    #     with open(loading_path) as f:
+    #         data = json.load(f)
+    #     # print(json.dumps(data, indent = 4, sort_keys=True))
+    #     for joints, attrs in data["objects"].items():
+    #         if joints in identifiers:
+    #             for attr_name, value in attrs["attrs"].items():
+    #                 if isinstance(value["value"], float):
+    #                     self.SetObjectAttribute(joints, attr_name, value["value"])
