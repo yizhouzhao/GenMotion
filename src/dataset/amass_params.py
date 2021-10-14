3aee0b740526fa92229baeaab7384d986cb6b662
Yizhou <yizhouzhao@g.ucla.edu>
2021-10-14 11:35:04 -0700

    update maya animation notes

---
+++ src/dataset/amass_params.py
@@ -0,0 +1,47 @@
+# Get skeleton information from https://meshcapade.wiki/SMPL#smpl-x
+SMPL_SKELETON = {
+    0: 'Pelvis',     3: 'Spine1',       6: 'Spine2',    9: 'Spine3',    12: 'Neck',     15: 'Head',
+     1: 'L_Hip',      4: 'L_Knee',       7: 'L_Ankle',  10: 'L_Foot',
+     2: 'R_Hip',      5: 'R_Knee',       8: 'R_Ankle',  11: 'R_Foot',
+    13: 'L_Collar',  16: 'L_Shoulder',  18: 'L_Elbow',  20: 'L_Wrist',
+    14: 'R_Collar',  17: 'R_Shoulder',  19: 'R_Elbow',  21: 'R_Wrist',
+    22: 'L_Hand',
+    23: 'R_Hand'
+}
+
+SMPL_H_SKELETON = {
+    0: 'Pelvis',     3: 'Spine1',       6: 'Spine2',    9: 'Spine3',    12: 'Neck',     15: 'Head',
+    1: 'L_Hip',      4: 'L_Knee',       7: 'L_Ankle',  10: 'L_Foot',
+    2: 'R_Hip',      5: 'R_Knee',       8: 'R_Ankle',  11: 'R_Foot',
+    13: 'L_Collar',  16: 'L_Shoulder',  18: 'L_Elbow',  20: 'L_Wrist',
+    14: 'R_Collar',  17: 'R_Shoulder',  19: 'R_Elbow',  21: 'R_Wrist',
+    22: 'lindex0',  23: 'lindex1',  24: 'lindex2',
+    25: 'lmiddle0', 26: 'lmiddle1', 27: 'lmiddle2',
+    28: 'lpinky0',  29: 'lpinky1',  30: 'lpinky2',
+    31: 'lring0',   32: 'lring1',   33: 'lring2',
+    34: 'lthumb0',  35: 'lthumb1',  36: 'lthumb2',
+    37: 'rindex0',  38: 'rindex1',  39: 'rindex2',
+    40: 'rmiddle0', 41: 'rmiddle1', 42: 'rmiddle2',
+    43: 'rpinky0',  44: 'rpinky1',  45: 'rpinky2',
+    46: 'rring0',   47: 'rring1',   48: 'rring2',
+    49: 'rthumb0',  50: 'rthumb1',  51: 'rthumb2'
+}
+
+SMPL_X_SKELTON = {
+    0: 'Pelvis',     3: 'Spine1',       6: 'Spine2',    9: 'Spine3',    12: 'Neck',     15: 'Head',
+    1: 'L_Hip',      4: 'L_Knee',       7: 'L_Ankle',  10: 'L_Foot',
+    2: 'R_Hip',      5: 'R_Knee',       8: 'R_Ankle',  11: 'R_Foot',
+    13: 'L_Collar',  16: 'L_Shoulder',  18: 'L_Elbow',  20: 'L_Wrist',
+    14: 'R_Collar',  17: 'R_Shoulder',  19: 'R_Elbow',  21: 'R_Wrist',
+    22: 'Jaw',      23: 'L_eye',    24: 'R_eye',
+    25: 'lindex0',  26: 'lindex1',  27: 'lindex2',
+    28: 'lmiddle0', 29: 'lmiddle1', 30: 'lmiddle2',
+    31: 'lpinky0',  32: 'lpinky1',  33: 'lpinky2',
+    34: 'lring0',   35: 'lring1',   36: 'lring2',
+    37: 'lthumb0',  38: 'lthumb1',  39: 'lthumb2',
+    40: 'rindex0',  41: 'rindex1',  42: 'rindex2',
+    43: 'rmiddle0', 44: 'rmiddle1', 45: 'rmiddle2',
+    46: 'rpinky0',  47: 'rpinky1',  48: 'rpinky2',
+    49: 'rring0',   50: 'rring1',   51: 'rring2',
+    52: 'rthumb0',  53: 'rthumb1',  54: 'rthumb2'
+}
