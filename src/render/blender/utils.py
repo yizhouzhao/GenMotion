328e0449c6f43b5b5ebb7d2d3f8672a970102345
Vince Ai <vinceai@outlook.com>
2021-10-16 20:57:40 -0700

    Add blender support for HDM05

--- src/render/blender/utils.py
+++ src/render/blender/utils.py
@@ -1,65 +1,9 @@
 import bpy
 from tqdm import tqdm
 import numpy as np
-
-# smpl-x skeleton dict
-SMPL_X_JOINTS = { 
-    0: 'pelvis',                       
-    1: 'left_hip',             
-    2: 'right_hip',
-    3: 'spine1', 
-    4: 'left_knee',      
-    5: 'right_knee',
-    6: 'spine2', 
-    7: 'left_ankle', 
-    8: 'right_ankle',
-    9: 'spine3',  
-    10: 'left_foot',
-    11: 'right_foot',
-    12: 'neck', 
-    13: 'left_collar',      
-    14: 'right_collar',
-    15: 'head',
-    16: 'left_shoulder', 
-    17: 'right_shoulder',
-    18: 'left_elbow',  
-    19: 'right_elbow',
-    20: 'left_wrist',  
-    21: 'right_wrist',
-    22: 'jaw',      
-    23: 'left_eye_smplhf',    
-    24: 'right_eye_smplhf',
-    25: 'left_index1',  
-    26: 'left_index2',  
-    27: 'left_index3',
-    28: 'left_middle1', 
-    29: 'left_middle2', 
-    30: 'left_middle3',
-    31: 'left_pinky1',  
-    32: 'left_pinky2',  
-    33: 'left_pinky3',
-    34: 'left_ring1',   
-    35: 'left_ring2',   
-    36: 'left_ring3',
-    37: 'left_thumb1',  
-    38: 'left_thumb2',  
-    39: 'left_thumb3',
-    40: 'right_index1',  
-    41: 'right_index2',  
-    42: 'right_index3',
-    43: 'right_middle1', 
-    44: 'right_middle2', 
-    45: 'right_middle3',
-    46: 'right_pinky1',  
-    47: 'right_pinky2',  
-    48: 'right_pinky3',
-    49: 'right_ring1',   
-    50: 'right_ring2',   
-    51: 'right_ring3',
-    52: 'right_thumb1',  
-    53: 'right_thumb2',  
-    54: 'right_thumb3'
-}
+from typing import Any, List, Optional
+from dataset.hdm05_params import ASF_JOINT2DOF
+from dataset.amass_params import SMPL_X_SKELTON2
 
 def import_fbx(file_path: str):
     bpy.ops.import_scene.fbx(filepath=file_path)
@@ -70,6 +14,27 @@ def import_smplx(gender: str):
 
 def clear_all_animation():
     bpy.context.active_object.animation_data_clear()
+
+def set_joint_location_keyframe(joint: Any, data: List[float], frame: int):
+    joint.location = data
+    joint.keyframe_insert(data_path="location", frame=frame)
+
+def set_joint_rotation_keyframe(joint: Any, data: List[float], frame: int, mode: str ="euler", axis: Optional[List[str]]=None):
+    if mode == "euler":
+        if axis:
+            for i in range(len(axis)):
+                if axis[i] == "rx":
+                    joint.rotation_euler[0] = data[i]
+                elif axis[i] == "ry":
+                    joint.rotation_euler[1] = data[i]
+                elif axis[i] == "rz":
+                    joint.rotation_euler[2] = data[i]
+        else:
+            joint.rotation_euler = data
+        joint.keyframe_insert(data_path="rotation_euler", frame=frame)
+    elif mode == "quaternion":
+        joint.rotation_quaternion = data
+        joint.keyframe_insert(data_path="rotation_quaternion", frame=frame)
 
 def set_amass_animation(data):
     # set frame properties
@@ -84,18 +49,34 @@ def set_amass_animation(data):
     character = bpy.data.objects["SMPLX-neutral"]
     root = character.pose.bones["root"]
     root.rotation_mode = "XYZ"
-    for i, joint_name in SMPL_X_JOINTS.items():
+    for i, joint_name in SMPL_X_SKELTON2.items():
         character.pose.bones[joint_name].rotation_mode = "XYZ"
         
     for frame in tqdm(range(total_frame)):
         # set root location and rotation
-        root.location = data["trans"][frame]
-        root.keyframe_insert(data_path="location", frame=frame)
-        root.rotation_euler = data["root_orient"][frame]
-        root.keyframe_insert(data_path="rotation_euler", frame=frame)
+        set_joint_location_keyframe(root, data["trans"][frame], frame)
+        set_joint_rotation_keyframe(root, data["root_orient"][frame], frame)
         
         # set skeleton joints
-        for i, joint_name in SMPL_X_JOINTS.items():
-            joint = character.pose.bones[joint_name]
-            joint.rotation_euler = data["poses"][frame][i * 3: (i + 1) * 3]
-            joint.keyframe_insert(data_path="rotation_euler", frame=frame)   
+        for i, joint_name in SMPL_X_SKELTON2.items():
+            set_joint_rotation_keyframe(character.pose.bones[joint_name], data["poses"][frame][i * 3: (i + 1) * 3], frame)  
+
+def set_amc_animation(amc_file_path: str):
+    with open(amc_file_path, "rb") as f:
+        cur_frame = 0
+        character = bpy.data.objects["Armature"]
+        character.select_set(True)
+        bpy.ops.object.mode_set(mode="POSE")
+        for line in tqdm(f.readlines()):
+            if line.strip().isdigit():
+                cur_frame = int(line)
+            elif cur_frame > 0 and cur_frame % 100 == 0:
+                data = line.decode("utf-8").strip().split()
+                joint_name = data[0]
+                joint = character.pose.bones[joint_name]
+                character.pose.bones[joint_name].rotation_mode = "XYZ"
+                if joint_name == "root":
+                    set_joint_location_keyframe(joint, np.float_(data[1:4]), cur_frame)
+                    set_joint_rotation_keyframe(joint, np.float_(data[4:]) * np.pi / 180, cur_frame)
+                else:
+                    set_joint_rotation_keyframe(joint, np.float_(data[1:]) * np.pi / 180, cur_frame, axis=ASF_JOINT2DOF[joint_name])
