4bd9d389d66698712ba546c7680d0b2939196b00
yizhouzhao <yizhou_zhao@berkeley.edu>
2022-08-24 23:42:00 -0700

    update bib

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,8 @@ extensions = [
     'nbsphinx',
 ]
 
+bibtex_bibfiles = ['refs.bib']
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
